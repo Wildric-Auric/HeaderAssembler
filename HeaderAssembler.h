eae4560cebadfb551a1a4659fccde3193f0540f0
Oussama <70033490+Wildric-Auric@users.noreply.github.com>
2023-11-22 15:36:55 +0100

    added comment about code origin

--- HeaderAssembler.h
+++ HeaderAssembler.h
@@ -10,6 +10,7 @@ enum class State {
 	Fail
 };
 
+//Code borrowed from NWengine utilities
 bool FileExists(std::string dir);
 std::string GetFileName(std::string path, std::string* bFilename = nullptr, std::string* bExtension = nullptr, std::string* bRoot = nullptr);
 std::vector<std::string> GetDirFiles(const std::string& directory, const std::string& extensionFilter = "");
